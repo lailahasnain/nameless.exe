dffdfa828cb04fef6eb6054af6bcb862c1ba6b97
Nicholas Perez-Aguilar <0687883@MY.SCCCD.EDU>
2019-10-29 21:32:22 -0700

    The Add Trip page UI has been completed.
    There is a dependency with hotels upon the location. Instead of having 40 hotels listed, the location will determine which hotels are to be listed in the dropdown menu for selection. This is done using Javascript.
    
    Pictures have been added for each respective hotel that can be chosen.

---
+++ Global_JS.js
@@ -0,0 +1,139 @@
+// JavaScript source code
+$(document).ready(function () {
+
+    $("#visiting_location").change(function () {
+
+        var el = $(this);
+
+        if (el.val() === "1")
+        {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Select an option ></option>"); // Used for having a placeholder that matches the other dropdowns
+            $("#hotel").append("   <option>Kimpton Hotel (Seattle)</option>");
+            $("#hotel").append("   <option>Sheraton Grand Seattle (Seattle)</option>");
+            $("#hotel").append("   <option>Grandy Hyatt Seattle (Seattle)</option>");
+            $("#hotel").append("   <option>Crowne Plaza (Seattle)</option>");
+            $("#hotel").append("   <option>Hyatt at Olive 8 (Seattle)</option>");
+        }
+        else if (el.val() === "2")
+        {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Select an option ></option>"); // Used for having a placeholder that matches the other dropdowns
+            $("#hotel").append("   <option>Shamrock Motel (Bellingham)</option>");
+            $("#hotel").append("   <option>Motel 6 (Bellingham)</option>");
+            $("#hotel").append("   <option>Chrysalis Inn and Spa (Bellingham)</option>");
+            $("#hotel").append("   <option>GuestHouse Inn (Bellingham)</option>");
+            $("#hotel").append("   <option>Holiday Inn (Bellingham)</option>");
+        }
+        else if (el.val() === "3")
+        {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Select an option ></option>"); // Used for having a placeholder that matches the other dropdowns
+            $("#hotel").append("   <option>Holiday Inn (Portland)</option>");
+            $("#hotel").append("   <option>Hyatt Place (Portland)</option>");
+            $("#hotel").append("   <option>Stanford Court (Portland)</option>");
+            $("#hotel").append("   <option>Travelodge (Portland)</option>");
+            $("#hotel").append("   <option>Travelodge by Wyndam (Portland)</option>");
+        }
+        else if (el.val() === "4")
+        {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Select an option ></option>"); // Used for having a placeholder that matches the other dropdowns
+            $("#hotel").append("   <option>Samesun Venice Beach (San Francisco)</option>");
+            $("#hotel").append("   <option>Value Inn (San Francisco)</option>");
+            $("#hotel").append("   <option>Luxe City Center Hotel (San Francisco)</option>");
+            $("#hotel").append("   <option>Motel 6 (San Francisco)</option>");
+            $("#hotel").append("   <option>Hotel Crowne Plaza (San Francisco)</option>");
+        }
+        else if (el.val() === "5")
+        {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Select an option ></option>"); // Used for having a placeholder that matches the other dropdowns
+            $("#hotel").append("   <option>Vacation Inn (Los Angeles)</option>");
+            $("#hotel").append("   <option>EZ 8 Motel (Los Angeles)</option>");
+            $("#hotel").append("   <option>Premier Inn (Los Angeles)</option>");
+            $("#hotel").append("   <option>Lux Uptown Apts. (Los Angeles)</option>");
+            $("#hotel").append("   <option>Hotel Embassy (Los Angeles)</option>");
+        }
+        else if (el.val() === "6")
+        {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Select an option ></option>"); // Used for having a placeholder that matches the other dropdowns
+            $("#hotel").append("   <option>Shamrock Motel (Phoenix)</option>");
+            $("#hotel").append("   <option>Motel 6 (Phoenix)</option>");
+            $("#hotel").append("   <option>Chrysalis Inn and Spa (Phoenix)</option>");
+            $("#hotel").append("   <option>GuestHouse Inn (Phoenix)</option>");
+            $("#hotel").append("   <option>Holiday Inn (Phoenix)</option>");
+        }
+        else if (el.val() === "7")
+        {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Select an option ></option>"); // Used for having a placeholder that matches the other dropdowns
+            $("#hotel").append("   <option>Tru (Las Vegas)</option>");
+            $("#hotel").append("   <option>The LINQ (Las Vegas)</option>");
+            $("#hotel").append("   <option>Travelodge (Las Vegas)</option>");
+            $("#hotel").append("   <option>Four Queens (Las Vegas)</option>");
+            $("#hotel").append("   <option>The Venetian (Las Vegas)</option>");
+        }
+        else if (el.val() === "8")
+        {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Select an option ></option>"); // Used for having a placeholder that matches the other dropdowns
+            $("#hotel").append("   <option>Circus Circus (Reno)</option>");
+            $("#hotel").append("   <option>Motel 6 (Reno)</option>");
+            $("#hotel").append("   <option>Sands Regency (Reno)</option>");
+            $("#hotel").append("   <option>Baymont Inn (Reno)</option>");
+            $("#hotel").append("   <option>Nugget (Reno)</option>");
+        }
+        else if (el.val() === "0") {
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel option:last-child").remove();
+            $("#hotel").append("   <option>< Please pick a location first ></option>");
+        }
+    });
+
+});
